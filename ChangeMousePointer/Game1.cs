ae766429b731defb0f78a5ce70b24b0bf179ab11
dlent1 <dave_meade@yahoo.com>
2022-03-18 22:19:52 -0500

    * Game1.cs:
    * Content.mgcb: Final code

--- ChangeMousePointer/Game1.cs
+++ ChangeMousePointer/Game1.cs
@@ -8,6 +8,13 @@ namespace ChangeMousePointer
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _mouseHand;
+        private Texture2D _goblin;
+        private Vector2 _goblinCoordinates;
+        private int _goblinLength;
+        private bool _handVisible;
+        private MouseState _mouseState;
+        
 
         public Game1()
         {
@@ -19,6 +26,9 @@ namespace ChangeMousePointer
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _goblinCoordinates = new Vector2(300, 300);
+            _goblinLength = 72;
+            _handVisible = false;
 
             base.Initialize();
         }
@@ -26,7 +36,8 @@ namespace ChangeMousePointer
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            _mouseHand = Content.Load<Texture2D>("mouseCursor");
+            _goblin = Content.Load<Texture2D>("Goblin2");
             // TODO: use this.Content to load your game content here
         }
 
@@ -36,6 +47,8 @@ namespace ChangeMousePointer
                 Exit();
 
             // TODO: Add your update logic here
+            _mouseState = Mouse.GetState();
+            HandleInput();
 
             base.Update(gameTime);
         }
@@ -45,8 +58,31 @@ namespace ChangeMousePointer
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin(SpriteSortMode.FrontToBack);
+            _spriteBatch.Draw(_goblin, _goblinCoordinates, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 0.1f);
+
+            if (_handVisible)
+                _spriteBatch.Draw(_mouseHand, new Vector2(_mouseState.X, _mouseState.Y), null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void HandleInput()
+        {
+            if (_mouseState.X >= _goblinCoordinates.X && _mouseState.X < _goblinCoordinates.X + _goblinLength
+                && _mouseState.Y >= _goblinCoordinates.Y && _mouseState.Y < _goblinCoordinates.Y + _goblinLength)
+            {
+                _handVisible = true;
+                IsMouseVisible = false;
+            }
+
+            else
+            {
+                _handVisible = false;
+                IsMouseVisible = true;
+            }
         }
     }
 }
